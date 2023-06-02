4550ae8040c549a3b8d707c60e13e02ffc1d7c97
Jonathan <jonathankarlinski57@gmail.com>
2023-06-02 17:21:29 -0300

    refactor: Arrumando indentação

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -2,7 +2,8 @@ def study_schedule(permanence_period, target_time):
     if target_time is None:
         return None
 
-    if any(type(period) != tuple or type(period[0]) != int or type(period[1]) != int for period in permanence_period):
+    if any(type(period) != tuple or type(period[0]) != int
+           or type(period[1]) != int for period in permanence_period):
         return None
 
     quantity = 0
