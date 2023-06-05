7f60f5a6d332f8ca3ef9725b54b7c011aed81dc4
Jonathan <jonathankarlinski57@gmail.com>
2023-06-05 18:16:56 -0300

    refactor: Arrumando indentação

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,12 +1,12 @@
 def is_palindrome_iterative(word):
     if len(word) != 2:
         return False
-    
+
     first_string, second_string = word
     if len(first_string) == 0 or len(second_string) == 0:
         return False
-    
+
     first_string = sorted(first_string.lower())
     second_string = sorted(second_string.lower())
-    
+
     return first_string == second_string
