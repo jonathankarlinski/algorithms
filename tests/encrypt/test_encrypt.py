d77b69bd751e0d11b765c3ed6848754aa3fe0995
Jonathan <jonathankarlinski57@gmail.com>
2023-06-05 14:08:08 -0300

    feat: test_encrypt_message

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,12 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+
+    assert encrypt_message("jonathan", 4) == "naht_anoj"
+    assert encrypt_message("jonathan", 5) == "tanoj_nah"
+    assert encrypt_message("jonathan", 9) == "nahtanoj"
+
+    with pytest.raises(TypeError, match="tipo inválido para key"):
+        encrypt_message(18, "jonathan")
