2e207e69be2af1c59730c2e378a72e87aafcd119
Jonathan <jonathankarlinski57@gmail.com>
2023-06-06 18:32:47 -0300

    feat: is_anagram

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,3 +1,15 @@
 def is_anagram(first_string, second_string):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not first_string or not second_string:
+        return False
+
+    first_string = sorted(first_string.lower())
+    second_string = sorted(second_string.lower())
+
+    return first_string == second_string
+
+
+def check_anagram(first_string, second_string):
+    sorted_first = ''.join(sorted(first_string.lower()))
+    sorted_second = ''.join(sorted(second_string.lower()))
+
+    return sorted_first, sorted_second, is_anagram(first_string, second_string)
