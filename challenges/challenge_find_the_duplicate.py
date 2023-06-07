74182509562a23673c0f4c0fe072c1baaee962f0
Jonathan <jonathankarlinski57@gmail.com>
2023-06-07 18:58:33 -0300

    feat: find_duplicate

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,14 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not nums or isinstance(nums, str) or len(nums) < 2:
+        return False
+
+    num_set = set()
+
+    for num in nums:
+        if not isinstance(num, int) or num < 1:
+            return False
+        elif num in num_set:
+            return num
+        else:
+            num_set.add(num)
+    return False
