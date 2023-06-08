144645febfb3b65f32f43501becf2ea47ebe12be
Jonathan <jonathankarlinski57@gmail.com>
2023-06-08 14:21:59 -0300

    refactor: Refatorando is_palindrome_iterative

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,12 +1,10 @@
 def is_palindrome_iterative(word):
-    if len(word) != 2:
+    if not word:
         return False
 
-    first_string, second_string = word
-    if len(first_string) == 0 or len(second_string) == 0:
-        return False
+    size = len(word)
+    for i in range(size // 2):
+        if word[i] != word[size - i - 1]:
+            return False
 
-    first_string = sorted(first_string.lower())
-    second_string = sorted(second_string.lower())
-
-    return first_string == second_string
+    return True
